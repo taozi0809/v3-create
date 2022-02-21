f8dbd1c171d0a73cde039d0c06ead967b0610b0e
taozi0809 <278446416@qq.com>
2022-02-21 09:34:44 +0000

    Deploying to gh-pages from  @ 7698e454041da027bd6119ae8a911a4d0772aee4 🚀

---
+++ assets/i18n.html.f6fa6f2b.js
@@ -0,0 +1,1 @@
+const e={key:"v-ecaf6aa8",path:"/guide/ui/i18n.html",title:"\u56FD\u9645\u5316",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1645435723e3,contributors:[{name:"zoutao",email:"278446416@qq.com",commits:1}]},filePathRelative:"guide/ui/i18n.md"};export{e as data};
